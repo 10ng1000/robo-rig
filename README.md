c6077ab5d0748f0d71e2391a5673dbf5d77fea2b
10ng1000 <2441622724@qq.com>
2023-05-02 14:25:37 +0800

    重构了代码逻辑，增加了二十四式太极拳的前两式

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Robot Rig
 An animated robot in C++ using the openGL and glut libraries.
 Creates a rig (skeleton hierarchy tree), draws a robot, then offers pre-programmed animations for demonstration purposes. Animations include the best Gangnam Style ever seen by humankind.
+
+# 机器人可视化大作业
+重构了代码逻辑，增加了二十四式太极拳的前两式动作。
--- robo_rig.cpp
+++ robo_rig.cpp
@@ -1,7 +1,7 @@
 /**************************************************
  *						  *
  * Animated Robot with OpenGL.			  *
- *					 	  *	
+ *					 	  *
  *       by					  *
  *					 	  *
  * Vlasis Gogousis [vgogousis@gmail.com]	  *
@@ -10,16 +10,15 @@
  * Aristotle University of Thessaloniki, 2014	  *
  *						  *
  **************************************************/
-
+//���������ע�Ͳ��ֶ��Ƕ������Ĵ���
+#define _CRT_SECURE_NO_WARNINGS
 #include <math.h>
 #include <stdio.h>
+#include <vector>
 #include <GL/glut.h>
 #include <stdlib.h>
 
-using namespace std;
-
-
-#define STEP 0.1
+#define STEP 0.05
 #define TORSO_HEIGHT 6.0
 #define TORSO_RADIUS 2.0
 #define UPPER_ARM_HEIGHT 3.0
@@ -42,11 +41,16 @@ using namespace std;
 
 #define NULL 0
 
+int win_width = 800;
+int win_height = 750;
 int option = -1;
 int animation = -3; //teleytaio animation
 int done = 0; //an oloklirwthike to teleytaio animation
-double rotate = 0;
-double horizontal = 0;
+double rotate = -20;
+double horizontal = 0.03 * STEP;
+//�������������ƶ���ǰ���ƶ�
+double vertical = -0.03 * STEP;
+double backward = 0;
 
 void head();
 void torso();
@@ -62,19 +66,47 @@ typedef struct treenode
 {
 	GLfloat m[16];
 	void(*f)();
-	struct treenode *sibling;
-	struct treenode *child;
+	struct treenode* sibling;
+	struct treenode* child;
 }treenode;
+
+//���task����װÿһ����ת����ϸ��
+class Task
+{
+public:
+	void(*f)();
+	bool isDone;
+	Task(void(*f)(), bool isDone) {
+		this->f = f;
+		this->isDone = isDone;
+	}
+};
+
+//��װö�٣��򻯲����Ĵ���
+enum Position
+{
+	BODY, NECK, HEAD, LUA, LLA, RUA, RLA, LUL, LLL, RUL, RLL,
+};
+
 
 typedef treenode* t_ptr;
 
+//������С
+static GLfloat steps[11] = { STEP, STEP, STEP ,STEP,STEP,STEP,STEP,STEP,STEP,STEP,STEP};
+
+//��ת��Ŀ�����
 static GLfloat theta[11] = { 0.0,0.0,0.0,180.0,0.0,180.0,0.0,
-180.0,0.0,180.0,0.0 }; /* initial joint angles */
+180.0,0.0,180.0,0.0};
+
+//�������ά��ת�Ķ���
+/*static GLfloat thetas[3][11] = {0.0,0.0,0.0,180.0,0.0,180.0,0.0,180.0,0.0,180.0,0.0,
+								0.0,0.0,0.0,180.0,0.0,180.0,0.0,180.0,0.0,180.0,0.0,
+								0.0,0.0,0.0,180.0,0.0,180.0,0.0,180.0,0.0,180.0,0.0}; */
 
 static GLint angle = 2;
 
-GLUquadricObj *t, *h, *lua, *lla, *rua, *rla, *lll, *rll, *rul, *lul;
-GLUquadricObj *relb, *lelb, *rknee, *lknee, *nk, *lhand, *rhand, *lfoot, *rfoot, *rsh, *lsh;
+GLUquadricObj* t, * h, * lua, * lla, * rua, * rla, * lll, * rll, * rul, * lul;
+GLUquadricObj* relb, * lelb, * rknee, * lknee, * nk, * lhand, * rhand, * lfoot, * rfoot, * rsh, * lsh;
 
 double size = 1.0;
 
@@ -89,7 +121,9 @@ void traverse(treenode* root)
 	glPushMatrix();
 	glMultMatrixf(root->m);
 	root->f();
-	if (root->child != NULL) traverse(root->child);
+	if (root->child != NULL) {
+		traverse(root->child);
+	}
 	glPopMatrix();
 	if (root->sibling != NULL) traverse(root->sibling);
 }
@@ -98,7 +132,7 @@ void torso()
 {
 	glPushMatrix();
 	glRotatef(-90.0, 1.0, 0.0, 0.0);
-	gluCylinder(t, TORSO_RADIUS / 1.2, TORSO_RADIUS, TORSO_HEIGHT, 10, 10);
+	gluCylinder(t, TORSO_RADIUS / 1.3, TORSO_RADIUS, TORSO_HEIGHT, 10, 10);
 	glPopMatrix();
 }
 
@@ -111,7 +145,7 @@ void head()
 	//glasses
 	glRotatef(-90.0, 1.0, 0.0, 0.0);
 	glTranslatef(0.0f, -0.4f, -0.15f);
-	gluCylinder(h, 0.9*HEAD_RADIUS, 0.9*HEAD_RADIUS, HEAD_HEIGHT / 5, 10, 10);
+	gluCylinder(h, 0.9 * HEAD_RADIUS, 0.9 * HEAD_RADIUS, HEAD_HEIGHT / 5, 10, 10);
 	glPopMatrix();
 }
 
@@ -244,6 +278,7 @@ void left_lower_leg()
 
 void right_upper_leg()
 {
+
 	glPushMatrix();
 	glRotatef(-90.0, 1.0, 0.0, 0.0);
 	gluCylinder(rul, UPPER_LEG_RADIUS, UPPER_LEG_RADIUS - 0.1, UPPER_LEG_HEIGHT, 10, 10);
@@ -265,14 +300,95 @@ void display(void)
 	glOrtho(-15, 15, -15, 15, -15, 15);
 	glRotatef(rotate, 0, 1, 0);
 	glMatrixMode(GL_MODELVIEW);
-
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 	glColor3f(0.4, 0.4, 0.4);
-
 	traverse(&torso_node);
 
 	glutSwapBuffers();
+}
+
+// ���øú�����ʹ���������˶�
+void act(enum Position pos ,double x, double y, double z) {
+	glPushMatrix();
+	switch (pos)
+	{
+	case RUL:
+		glRotatef(steps[9], x, y, z);
+		glMultMatrixf(rul_node.m);
+		theta[9] += steps[9];
+		glGetFloatv(GL_MODELVIEW_MATRIX, rul_node.m);
+		break;
+	case RLL:
+		glRotatef(steps[10], x, y, z);
+		glMultMatrixf(rll_node.m);
+		theta[10] += steps[10];
+		glGetFloatv(GL_MODELVIEW_MATRIX, rll_node.m);
+		break;
+	case RUA:
+		glRotatef(steps[5], x, y, z);
+		glMultMatrixf(rua_node.m);
+		theta[5] += steps[5];
+		glGetFloatv(GL_MODELVIEW_MATRIX, rua_node.m);
+		break;
+	case RLA:
+		glRotatef(steps[6], x, y, z);
+		glMultMatrixf(rla_node.m);
+		theta[6] += steps[6];
+		glGetFloatv(GL_MODELVIEW_MATRIX, rla_node.m);
+		break;
+	case LUA:
+		glRotatef(steps[3], x, y, z);
+		glMultMatrixf(lua_node.m);
+		theta[3] += steps[3];
+		glGetFloatv(GL_MODELVIEW_MATRIX, lua_node.m);
+		break;
+	case LLA:
+		glRotatef(steps[4], x, y, z);
+		glMultMatrixf(lla_node.m);
+		theta[4] += steps[4];
+		glGetFloatv(GL_MODELVIEW_MATRIX, lla_node.m);
+		break;
+	case LUL:
+		glRotatef(steps[7], x, y, z);
+		glMultMatrixf(lul_node.m);
+		theta[7] += steps[7];
+		glGetFloatv(GL_MODELVIEW_MATRIX, lul_node.m);
+		break;
+	case LLL:
+		glRotatef(steps[8], x, y, z);
+		glMultMatrixf(lll_node.m);
+		theta[8] += steps[8];
+		glGetFloatv(GL_MODELVIEW_MATRIX, lll_node.m);
+		break;
+	default:
+		break;
+	}
+	glPopMatrix();
+}
+
+// ���øú�����ʹ���������˶�
+void bodyAct(int x=0, int y=0, int z=0) {
+	glPushMatrix();
+	glTranslatef(horizontal, vertical, backward);
+	glRotatef(steps[0], x, y, z);
+	glMultMatrixf(torso_node.m);
+	theta[0] += steps[0];
+	glGetFloatv(GL_MODELVIEW_MATRIX, torso_node.m);
+	glPopMatrix();
+}
+
+// ���ò�����Ŀ��Ƕ�
+void resetAngle() {
+	for (int i = 0; i < 11; i++) {
+		theta[i] = 0;
+		steps[i] = STEP;
+	}
+	steps[0] = 0;
+	horizontal = 0.03 * STEP;
+	//�������������ƶ���ǰ���ƶ�
+	vertical = -0.02 * STEP;
+	backward = 0;
 }
 
 void inverseKinematics()
@@ -284,7 +400,7 @@ void inverseKinematics()
 		{
 			theta[5] += STEP;
 			theta[3] += STEP;
-			theta[1] -= 0.2*STEP;
+			theta[1] -= 0.2 * STEP;
 		}
 		else animation = option; //an exei antistrafei tote eimaste stin arxiki thesi kai to neo animation einai to option
 
@@ -301,7 +417,7 @@ void inverseKinematics()
 		glGetFloatv(GL_MODELVIEW_MATRIX, lua_node.m);
 
 		glLoadIdentity();
-		glTranslatef(0.0, TORSO_HEIGHT - 0.25*NECK_HEIGHT, 0.0);
+		glTranslatef(0.0, TORSO_HEIGHT - 0.25 * NECK_HEIGHT, 0.0);
 		glRotatef(theta[1], 1.0, 0.0, 0.0);
 		glGetFloatv(GL_MODELVIEW_MATRIX, nk_node.m);
 
@@ -318,7 +434,7 @@ void inverseKinematics()
 		glPushMatrix();
 
 		glLoadIdentity();
-		glTranslatef(TORSO_RADIUS / 2, 0.1*UPPER_LEG_HEIGHT, 0.0);
+		glTranslatef(TORSO_RADIUS / 2, 0.1 * UPPER_LEG_HEIGHT, 0.0);
 		glRotatef(theta[9], 1.0, 0.0, 0.0);
 		glGetFloatv(GL_MODELVIEW_MATRIX, rul_node.m);
 
@@ -340,18 +456,18 @@ void inverseKinematics()
 			theta[10] -= STEP;
 			theta[7] += STEP;
 			theta[8] -= STEP;
-			theta[5] -= 1.3*STEP;
+			theta[5] -= 1.3 * STEP;
 			theta[6] += STEP;
 			theta[3] += STEP;
 			theta[4] -= STEP;
-			horizontal -= 0.03*STEP;
+			horizontal -= 0.03 * STEP;
 		}
 		else animation = option;
 
 		glPushMatrix();
 
 		glLoadIdentity();
-		glTranslatef(TORSO_RADIUS / 2, 0.1*UPPER_LEG_HEIGHT, 0.0);
+		glTranslatef(TORSO_RADIUS / 2, 0.1 * UPPER_LEG_HEIGHT, 0.0);
 		glRotatef(theta[9], 1.0, 0.0, -1.0);
 		glGetFloatv(GL_MODELVIEW_MATRIX, rul_node.m);
 
@@ -397,7 +513,7 @@ void inverseKinematics()
 			theta[6] += STEP;
 			theta[3] += STEP;
 			theta[4] -= STEP;
-			horizontal -= 0.03*STEP;
+			horizontal -= 0.03 * STEP;
 		}
 		else animation = option;
 
@@ -424,7 +540,7 @@ void inverseKinematics()
 		glGetFloatv(GL_MODELVIEW_MATRIX, lla_node.m);
 
 		glLoadIdentity();
-		glTranslatef(TORSO_RADIUS / 2, 0.1*UPPER_LEG_HEIGHT, 0.0);
+		glTranslatef(TORSO_RADIUS / 2, 0.1 * UPPER_LEG_HEIGHT, 0.0);
 		glRotatef(theta[9], 1.0, 0.0, 0.0);
 		glGetFloatv(GL_MODELVIEW_MATRIX, rul_node.m);
 
@@ -434,7 +550,7 @@ void inverseKinematics()
 		glGetFloatv(GL_MODELVIEW_MATRIX, rll_node.m);
 
 		glLoadIdentity();
-		glTranslatef(-TORSO_RADIUS / 2, 0.1*UPPER_LEG_HEIGHT, 0.0);
+		glTranslatef(-TORSO_RADIUS / 2, 0.1 * UPPER_LEG_HEIGHT, 0.0);
 		glRotatef(theta[7], 1.0, 0.0, 0.0);
 		glGetFloatv(GL_MODELVIEW_MATRIX, lul_node.m);
 
@@ -453,10 +569,289 @@ void inverseKinematics()
 	}
 }
 
+//����
+// һ��ʼ���Ȳ��˶�
+Task qishi0 = Task([]() {
+		//����̧��
+		//�˶��Ƕ������Լ��ؽ��˶��ٶ�
+		//Ϊ�˼���Դ���룬���Ǵ�180�ȿ�ʼ
+		steps[9] = -STEP;
+		steps[10] = 1.25 * STEP;
+		if (theta[9] <= 140.0)
+		{
+			resetAngle();
+			qishi0.isDone = true;
+		}
+		//�˶���λ����
+		act(RUL, 1, 0, 0);
+		act(RLL, 1, 0, 0);
+	}, false);
+Task qishi1 = Task([]() {
+	//��������
+	//�˶��Ƕ������Լ��ؽ��˶��ٶ�
+	steps[10] = 0.8 * STEP;
+	if (theta[9] >= 25.0)
+	{
+		resetAngle();
+		qishi1.isDone = true;
+	}
+	//�˶���λ����
+	act(RUL, 1, 0, 1);
+	act(RLL, -1, 0, 0);
+	}, false);
+Task qishi2 = Task([]() {
+	//�˶��Ƕ������Լ��ؽ��˶��ٶ�
+	steps[10] = 1.2 * STEP;
+	vertical = -0.01 * STEP;
+	if (theta[9] >= 25.0)
+	{
+		resetAngle();
+		qishi2.isDone = true;
+	}
+	//�˶���λ����
+	act(RUL, 1, 0, 0);
+	act(RLL, -1, 0, 0);
+	}, false);
+Task qishi3 = Task([]() {
+	//�˶��Ƕ������Լ��ؽ��˶��ٶ�
+	if (theta[7] >= 15.0)
+	{
+		resetAngle();
+		qishi3.isDone = true;
+	}
+	//�˶���λ����
+	act(LUL, 0, 0, -1);
+	bodyAct();
+	}, false);
+// ��������ʼ�����ֱ��˶�
+Task qishi4 = Task([]() {
+	//�˶��Ƕ������Լ��ؽ��˶��ٶ�
+	if (theta[3] >= 80.0)
+	{
+		resetAngle();
+		qishi4.isDone = true;
+	}
+	//�˶���λ����
+	act(LUA, -1, 0, 0);
+	act(RUA, -1, 0, 0);
+	}, false);
+Task qishi5 = Task([]() {
+	//�˶��Ƕ������Լ��ؽ��˶��ٶ�
+	steps[3] = 0.2 * STEP;
+	steps[5] = 0.2 * STEP;
+	if (theta[4] >= 50.0)
+	{
+		resetAngle();
+		qishi5.isDone = true;
+	}
+	//�˶���λ����
+	act(LLA, -1, 0, 0);
+	act(RLA, -1, 0, 0);
+	act(LUA, 1, 0, 0);
+	act(RUA, 1, 0, 0);
+	}, false);
+Task qishi6 = Task([]() {
+	//�˶��Ƕ������Լ��ؽ��˶��ٶ�
+	steps[4] = 0.8 * STEP;
+	steps[6] = 0.8 * STEP;
+	backward = 0.00001 * STEP;
+	horizontal = 0;
+	if (theta[7] >= 35.0) {
+		steps[7] = 0;
+		steps[8] = 0;
+		steps[9] = 0;
+		steps[10] = 0;
+		vertical = 0;
+		backward = 0.00001 * STEP;
+		horizontal = 0;
+	}
+	if (theta[4] >= 35.0) {
+		steps[4] = 0;
+		steps[6] = 0;
+	}
+	if (theta[3] >= 60.0)
+	{
+		resetAngle();
+		qishi6.isDone = true;
+	}
+	//�˶���λ����
+	act(LUA, 1, 0, 0);
+	act(RUA, 1, 0, 0);
+	act(LLA, 1, 0, 0);
+	act(RLA, 1, 0, 0);
+	act(LUL, -1, 0, 0);
+	act(RUL, -1, 0, 0);
+	act(LLL, 1, 0, 0);
+	act(RLL, 1, 0, 0);
+	bodyAct();
+	}, false);
+
+//Ұ�����
+Task yema0 = Task([]() {
+	//�˶��Ƕ������Լ��ؽ��˶��ٶ�
+	//steps[0] = 0.3 * STEP;
+	steps[3] = 1.5 * STEP;
+	steps[4] = 0.6 *STEP;
+	steps[5] = 0.8 * STEP;
+	steps[6] = 0.15 * STEP;
+	steps[7] = STEP;
+	steps[8] = 0.8* STEP;
+	steps[9] = STEP;
+	steps[10] =  STEP;
+	horizontal = -0.03 * STEP;
+	vertical = 0.001 * STEP;
+	if (theta[7] > 35) {
+		steps[7] = 0;
+		steps[8] = 0;
+		steps[9] = 0;
+		steps[10] = 0;
+		horizontal = 0;
+		vertical = 0;
+	}
+	if (theta[5] >= 35) {
+		steps[5] = 0;
+		steps[6] = 0;
+	}
+	if (theta[3] >= 90.0)
+	{
+		resetAngle();
+		yema0.isDone = true;
+	}
+	//�˶���λ����
+	act(LUA, -0.7, 0, -1);
+	act(LLA, 0, 0, -1);
+	act(RUA, 0.8, 0, 1);
+	act(RLA, 0, 0, -1);
+	act(LUL, 1, 0, 1);
+	act(LLL, 0, 0, 1);
+	act(RUL, 1, 0, 1);
+	act(RLL, -1, 0, 1);
+	bodyAct(0,1,0);
+	}, false);
+Task yema1 = Task([]() {
+	//�˶��Ƕ������Լ��ؽ��˶��ٶ�
+	steps[3] = 1 * STEP;
+	steps[4] = 1.2 * STEP;
+	steps[5] = 0.5*STEP;
+	steps[6] = 0.3*STEP;
+	steps[9] = -0.2 * STEP;
+	steps[10] = STEP;
+	if (theta[10] >= 70.0)
+	{
+		resetAngle();
+		yema1.isDone = true;
+	}
+	//�˶���λ����
+	act(LUA, -0.3, 0, 1);
+	act(LLA, 0.3, 0, -1);
+	act(RUA, -0.6, 0, -1);
+	act(RLA, 0, 0, 1);
+	act(RUL, 1, 0, 0);
+	act(RLL, 0, 0, 1);
+	}, false);
+Task yema2 = Task([]() {
+	//�˶��Ƕ������Լ��ؽ��˶��ٶ�
+	steps[0] = 0.8 * STEP;
+	steps[3] = 1 * STEP;
+	steps[4] = 1.2 * STEP;
+	steps[5] = 0.5 * STEP;
+	steps[6] = 0.5 * STEP;
+	steps[7] = 0.2 * STEP;
+	steps[8] = 0.6 * STEP;
+	steps[9] = 0.2 * STEP;
+	steps[10] = STEP;
+	vertical = -0.012 * STEP;
+	horizontal = 0;
+	if (theta[10] >= 90.0)
+	{
+		resetAngle();
+		yema2.isDone = true;
+	}
+	//�˶���λ����
+	//act(LUA, -0.3, 0, 1);
+	//act(LLA, 0.3, 0, -1);
+	act(RUA, -1, 0, 0);
+	act(RLA, 1, 0, 1);
+	act(LUL, -1, 0, 0);
+	act(LLL, 1, 0, 0);
+	act(RUL, -1, 0, -1);
+	act(RLL, -0.5, 0, -1);
+	bodyAct(0, 1, 0);
+	}, false);
+Task yema3 = Task([]() {
+	//�˶��Ƕ������Լ��ؽ��˶��ٶ�
+	steps[0] = 0;
+	steps[3] = 1 * STEP;
+	steps[4] = 1.2 * STEP;
+	steps[5] = 0.6 * STEP;
+	steps[6] = 1 * STEP;
+	steps[7] = 1 * STEP;
+	steps[8] = 1.2 * STEP;
+	steps[9] = 0.5 * STEP;
+	steps[10] = STEP;
+	vertical = 0;
+	horizontal = 0;
+	backward = -0.03 * STEP;
+	if (theta[10] >= 70.0)
+	{
+		resetAngle();
+		yema3.isDone = true;
+	}
+	//�˶���λ����
+	act(LUA, 1, 0, -0.3);
+	act(LLA, -0.3, 0, 1);
+	act(RUA, -0.7, 0, 1);
+	act(RLA, -0.5, 0, -1);
+	act(LUL, 1, 0, -0.2);
+	act(LLL, -1, 0, -0.4);
+	act(RUL, -1, 0, -0.5);
+	act(RLL, 1, 0, 0);
+	bodyAct(0, 0, 0);
+	}, false);
+
+
 void idle()
 {
 	switch (option)
 	{
+	case 9: {
+		//����
+		std::vector<Task*> taskList;
+		taskList.push_back(&qishi0);
+		taskList.push_back(&qishi1);
+		taskList.push_back(&qishi2);
+		taskList.push_back(&qishi3);
+		taskList.push_back(&qishi4);
+		taskList.push_back(&qishi5);
+		taskList.push_back(&qishi6);
+		Task* undone = nullptr;
+		//�ӵ�һ��δ��ɵ�����ʼִ��
+		for (auto t : taskList) {
+			if (!t->isDone) {
+				undone = t;
+				break;
+			}
+		}
+		if (undone != nullptr) undone->f();
+		break; }
+	case 10: {
+		//Ұ�����
+		std::vector<Task*> taskList;
+		taskList.push_back(&yema0);
+		taskList.push_back(&yema1);
+		taskList.push_back(&yema2);
+		taskList.push_back(&yema3);
+		Task* undone = nullptr;
+		//�ӵ�һ��δ��ɵ�����ʼִ��
+		for (auto t : taskList) {
+			if (!t->isDone) {
+				undone = t;
+				break;
+			}
+		}
+		if (undone != nullptr) undone->f();
+		break;
+	}
 	case 0:
 		if (animation != option) inverseKinematics(); //an to teleytaio animation einai diaforetiko antistrepse to
 		else if (!done) //kane animation mexri na oloklirwthei
@@ -465,7 +860,7 @@ void idle()
 			{
 				theta[5] -= STEP;
 				theta[3] -= STEP;
-				theta[1] += 0.2*STEP;
+				theta[1] += 0.2 * STEP;
 			}
 			else done = 1;
 
@@ -482,7 +877,7 @@ void idle()
 			glGetFloatv(GL_MODELVIEW_MATRIX, lua_node.m);
 
 			glLoadIdentity();
-			glTranslatef(0.0, TORSO_HEIGHT - 0.25*NECK_HEIGHT, 0.0);
+			glTranslatef(0.0, TORSO_HEIGHT - 0.25 * NECK_HEIGHT, 0.0);
 			glRotatef(theta[1], 1.0, 0.0, 0.0);
 			glGetFloatv(GL_MODELVIEW_MATRIX, nk_node.m);
 
@@ -503,7 +898,7 @@ void idle()
 			glPushMatrix();
 
 			glLoadIdentity();
-			glTranslatef(TORSO_RADIUS / 2, 0.1*UPPER_LEG_HEIGHT, 0.0);
+			glTranslatef(TORSO_RADIUS / 2, 0.1 * UPPER_LEG_HEIGHT, 0.0);
 			glRotatef(theta[9], 1.0, 0.0, 0.0);
 			glGetFloatv(GL_MODELVIEW_MATRIX, rul_node.m);
 
@@ -532,18 +927,18 @@ void idle()
 				theta[10] += STEP;
 				theta[7] -= STEP;
 				theta[8] += STEP;
-				theta[5] += 1.3*STEP;
+				theta[5] += 1.3 * STEP;
 				theta[6] -= STEP;
 				theta[3] -= STEP;
 				theta[4] += STEP;
-				horizontal += 0.03*STEP;
+				horizontal += 0.03 * STEP;
 			}
 			else animation = -1;
 
 			glPushMatrix();
 
 			glLoadIdentity();
-			glTranslatef(TORSO_RADIUS / 2, 0.1*UPPER_LEG_HEIGHT, 0.0);
+			glTranslatef(TORSO_RADIUS / 2, 0.1 * UPPER_LEG_HEIGHT, 0.0);
 			glRotatef(theta[9], 1.0, 0.0, -1.0);
 			glGetFloatv(GL_MODELVIEW_MATRIX, rul_node.m);
 
@@ -593,7 +988,7 @@ void idle()
 				theta[6] -= STEP;
 				theta[3] -= STEP;
 				theta[4] += STEP;
-				horizontal += 0.03*STEP;
+				horizontal += 0.03 * STEP;
 			}
 			else animation = -2;
 
@@ -620,7 +1015,7 @@ void idle()
 			glGetFloatv(GL_MODELVIEW_MATRIX, lla_node.m);
 
 			glLoadIdentity();
-			glTranslatef(TORSO_RADIUS / 2, 0.1*UPPER_LEG_HEIGHT, 0.0);
+			glTranslatef(TORSO_RADIUS / 2, 0.1 * UPPER_LEG_HEIGHT, 0.0);
 			glRotatef(theta[9], 1.0, 0.0, 0.0);
 			glGetFloatv(GL_MODELVIEW_MATRIX, rul_node.m);
 
@@ -630,7 +1025,7 @@ void idle()
 			glGetFloatv(GL_MODELVIEW_MATRIX, rll_node.m);
 
 			glLoadIdentity();
-			glTranslatef(-TORSO_RADIUS / 2, 0.1*UPPER_LEG_HEIGHT, 0.0);
+			glTranslatef(-TORSO_RADIUS / 2, 0.1 * UPPER_LEG_HEIGHT, 0.0);
 			glRotatef(theta[7], 1.0, 0.0, 0.0);
 			glGetFloatv(GL_MODELVIEW_MATRIX, lul_node.m);
 
@@ -638,6 +1033,31 @@ void idle()
 			glTranslatef(0.0, KNEE_RADIUS / 2, 0.0);
 			glRotatef(theta[8], 1.0, 0.0, 0.0);
 			glGetFloatv(GL_MODELVIEW_MATRIX, lll_node.m);
+
+			glLoadIdentity();
+			glTranslatef(0, -horizontal, -horizontal);
+			glGetFloatv(GL_MODELVIEW_MATRIX, torso_node.m);
+
+			glPopMatrix();
+		}
+		break;
+	case 8:
+		//�������
+		if (!done)
+		{
+			//ֱ���ϹؽڵĽǶȵ�110��
+			if (theta[3] > 110.0)
+			{
+				theta[3] -= STEP;
+				theta[4] += STEP;
+			}
+			else done = 1;
+
+			glPushMatrix();
+
+			//����act�����Ƿ���ã��Լ����Թؽڵ��˶���
+			act(LUL, 1, 0, 0);
+			act(LLL, 1, 0, 0);
 
 			glLoadIdentity();
 			glTranslatef(0, -horizontal, -horizontal);
@@ -760,7 +1180,7 @@ void myinit()
 	torso_node.child = &nk_node;
 
 	glLoadIdentity();
-	glTranslatef(0.0, TORSO_HEIGHT - 0.25*NECK_HEIGHT, 0.0);
+	glTranslatef(0.0, TORSO_HEIGHT - 0.25 * NECK_HEIGHT, 0.0);
 	glRotatef(theta[1], 1.0, 0.0, 0.0);
 	glGetFloatv(GL_MODELVIEW_MATRIX, nk_node.m);
 	nk_node.f = neck;
@@ -768,21 +1188,21 @@ void myinit()
 	nk_node.child = &head_node;
 
 	glLoadIdentity();
-	glTranslatef(-(TORSO_RADIUS + UPPER_ARM_RADIUS), 0.9*TORSO_HEIGHT, 0.0);
+	glTranslatef(-(TORSO_RADIUS + UPPER_ARM_RADIUS), 0.9 * TORSO_HEIGHT, 0.0);
 	glGetFloatv(GL_MODELVIEW_MATRIX, lsh_node.m);
 	lsh_node.f = leftShoulder;
 	lsh_node.sibling = &rsh_node;
 	lsh_node.child = &lua_node;
 
 	glLoadIdentity();
-	glTranslatef(TORSO_RADIUS + UPPER_ARM_RADIUS, 0.9*TORSO_HEIGHT, 0.0);
+	glTranslatef(TORSO_RADIUS + UPPER_ARM_RADIUS, 0.9 * TORSO_HEIGHT, 0.0);
 	glGetFloatv(GL_MODELVIEW_MATRIX, rsh_node.m);
 	rsh_node.f = rightShoulder;
 	rsh_node.sibling = &lul_node;
 	rsh_node.child = &rua_node;
 
 	glLoadIdentity();
-	glTranslatef(0.0, 0.75*NECK_HEIGHT, 0.0);
+	glTranslatef(0.0, 0.75 * NECK_HEIGHT, 0.0);
 	glRotatef(theta[2], 0.0, 1.0, 0.0);
 	glGetFloatv(GL_MODELVIEW_MATRIX, head_node.m);
 	head_node.f = head;
@@ -822,7 +1242,7 @@ void myinit()
 	relb_node.child = &rla_node;
 
 	glLoadIdentity();
-	glTranslatef(-TORSO_RADIUS / 2, 0.1*UPPER_LEG_HEIGHT, 0.0);
+	glTranslatef(-TORSO_RADIUS / 2, 0.1 * UPPER_LEG_HEIGHT, 0.0);
 	glRotatef(theta[7], 1.0, 0.0, 0.0);
 	glGetFloatv(GL_MODELVIEW_MATRIX, lul_node.m);
 	lul_node.f = left_upper_leg;
@@ -830,7 +1250,7 @@ void myinit()
 	lul_node.child = &lknee_node;
 
 	glLoadIdentity();
-	glTranslatef(TORSO_RADIUS / 2, 0.1*UPPER_LEG_HEIGHT, 0.0);
+	glTranslatef(TORSO_RADIUS / 2, 0.1 * UPPER_LEG_HEIGHT, 0.0);
 	glRotatef(theta[9], 1.0, 0.0, 0.0);
 	glGetFloatv(GL_MODELVIEW_MATRIX, rul_node.m);
 	rul_node.f = right_upper_leg;
@@ -916,12 +1336,12 @@ void myinit()
 	glLoadIdentity();
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(600, 600);
-	glutCreateWindow("Crazy Robot");
+	glutInitWindowSize(win_width, win_height);
+	glutCreateWindow("Taichi Robot");
 	myinit();
 	glutReshapeFunc(myReshape);
 	glutIdleFunc(idle);
@@ -930,15 +1350,17 @@ int main(int argc, char **argv)
 	glutCreateMenu(menu);
 	glutAddMenuEntry(" Raise Hands ", 0);
 	glutAddMenuEntry(" Raise Leg ", 1);
-	glutAddMenuEntry(" Dance ", 7);
-	glutAddMenuEntry(" Gangnam Style Dance ", 6);
 	glutAddMenuEntry(" Reset ", 2);
 	glutAddMenuEntry(" Left View ", 3);
 	glutAddMenuEntry(" Front View ", 4);
 	glutAddMenuEntry(" 3/4 View ", 5);
-
+	glutAddMenuEntry(" Gangnam Style Dance ", 6);
+	glutAddMenuEntry(" Dance ", 7);
+	glutAddMenuEntry(" ���� ", 8);
+	glutAddMenuEntry(" ���� ", 9);
+	glutAddMenuEntry(" Ұ����� ", 10);
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
 
 	glutMainLoop();
 	return 0;
-}
+        }
